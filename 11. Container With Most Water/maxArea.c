d8d2e24aab36a6b65ec02bca07ba6303a429c59c
KlimovVladimir <vl2598@mail.ru>
2026-04-16 15:54:41 +0700

    #11 Clean code

--- 11. Container With Most Water/maxArea.c
+++ 11. Container With Most Water/maxArea.c
@@ -3,16 +3,7 @@ int maxArea(int* height, int heightSize) {
     int start = 0;
     int end = heightSize - 1;
 
-    // for (int i = 0; i < heightSize - 1; i++) {
-    //     for (int j = i + 1; j < heightSize; j++) {
-    //         int min = height[i] < height[j] ? height[i] : height[j];
-    //         int square = min * (j - i);
-    //         if (square > max)
-    //             max = square;
-    //     }
-    // }
-
-    for (int i = 0; i < heightSize; i++) {
+    while (start < end) {
         int min = height[start] < height[end] ? height[start] : height[end];
         int square = min * (end - start);
         if (square > max)
