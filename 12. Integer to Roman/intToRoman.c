77af7b6af323ea11d432b5702ecff8658303c305
KlimovVladimir <vl2598@mail.ru>
2026-04-17 17:04:45 +0700

    #12 Add Integer to Roman task

---
+++ 12. Integer to Roman/intToRoman.c
@@ -0,0 +1,69 @@
+char* intToRoman(int num) {
+    int power = (int)log10(num);
+    char* oneList = "MCXI";
+    char* fiveList = "_DLV";
+
+    oneList = oneList + (3 - power);
+    fiveList = fiveList + (3 - power);
+
+    char* result = (char*)malloc(sizeof(char) * 16);
+    memset(result, 0, 16);
+
+    char* curr = result;
+
+    while (num > 0) {
+
+        int digit = num / (int)pow(10, power);
+
+        if (power == 3) {
+            for (int i = 0; i < digit; i++) {
+                *curr = *oneList;
+                curr++;
+            }
+            num -= digit * (int)pow(10, power);
+            power--;
+            oneList++;
+            fiveList++;
+            continue;
+        }
+
+        if (digit == 5) {
+            *curr = *fiveList;
+            curr++;
+        }
+        else if (digit == 4) {
+            *curr = *oneList;
+            curr++;
+            *curr = *fiveList;
+            curr++;
+        }
+        else if (digit == 9) {
+            *curr = *oneList;
+            curr++;
+            *curr = *(oneList - 1);
+            curr++;
+        }
+        else if (digit <= 3) {
+            for (int i = 0; i < digit; i++) {
+                *curr = *oneList;
+                curr++;
+            }
+        }
+        else if (digit >= 6) {
+            *curr = *fiveList;
+            curr++;
+            for (int i = 0; i < (digit - 5); i++) {
+                *curr = *oneList;
+                curr++;
+            }
+        }
+
+        num -= digit * (int)pow(10, power);
+        power--;
+        oneList++;
+        fiveList++;
+    }
+
+
+    return result;
+}
