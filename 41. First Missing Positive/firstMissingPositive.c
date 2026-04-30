7f62e2dd30e5f451e4e12f344dc8af0129f25ff0
KlimovVladimir <vl2598@mail.ru>
2026-04-30 16:37:19 +0700

    #41 Add First Missing Positive task

---
+++ 41. First Missing Positive/firstMissingPositive.c
@@ -0,0 +1,19 @@
+int comp(const void *a, const void *b) {
+    if (*(int *)a == INT_MIN || *(int *)b == INT_MIN)
+        return INT_MIN;
+    return (*(int *)a - *(int *)b);
+}
+
+int firstMissingPositive(int* nums, int numsSize) {
+    int result = 1;
+
+    qsort(nums, numsSize, sizeof(nums[0]), comp);
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == result) {
+            result = nums[i] + 1;
+        }
+    }
+
+    return result;
+}
